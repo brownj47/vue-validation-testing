51302006a7207f13d1a825628acc2521951725bf
Justus B <justus.c.brown@gmail.com>
2023-01-20 10:42:29 -0800

    validation works

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <template>
   <div id="app">
     <Form @submit="onSubmit">
-      <Field type="email" name="email" />
+      <Field type="email" name="email" :rules="validateEmail" />
+      <ErrorMessage name="email"/>
       <button>Sign up for newsletter</button>
     </Form>
   </div>
 </template>
 
 <script>
-import { Form, Field } from "vee-validate"
+import { Form, Field, ErrorMessage } from "vee-validate"
 
 export default {
   components: {
     Form,
-    Field
+    Field,
+    ErrorMessage
   },
   methods: {
-    onSubmit() {
-      console.log('Submitted');
+    onSubmit(values) {
+      console.log(values);
     },
+    validateEmail(value){
+      if (!value){
+        return "this field is required";
+      }
+      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+      if (!regex.test(value)) {
+        return 'This field must be a valid email';
+      }
+      // All is good
+      return true;
+    }
   },
 }
 </script>
