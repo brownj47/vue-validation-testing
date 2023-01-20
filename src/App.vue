7b1917eec242c58b6cf1f825c46008a5be387b08
Justus B <justus.c.brown@gmail.com>
2023-01-20 10:08:30 -0800

    change file structure for clarity

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Form @submit="onSubmit">
+      <Field type="email" name="email" />
+      <button>Sign up for newsletter</button>
+    </Form>
+  </div>
+</template>
+
+<script>
+import { Form, Field } from "vee-validate"
+
+export default {
+  components: {
+    Form,
+    Field
+  },
+  methods: {
+    onSubmit() {
+      console.log('Submitted');
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
